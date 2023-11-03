f0af15f5b22db5566dbaaa4010418c29769a579f
Anestezia-zip <plekatybtc@gmail.com>
2023-11-03 17:03:42 +0000

    Refactored blog views.py

--- blog/views.py
+++ blog/views.py
@@ -14,6 +14,13 @@ class UserPostListView(generic.ListView):
     context_object_name = 'posts_list'
     paginate_by = 6
 
-    def get_queryset(self):
+    # def get_queryset(self):
+    #     user = get_object_or_404(User, username=self.kwargs.get('username'))
+    #     return Post.objects.filter(author=user).order_by('-date_created')
+
+    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
         user = get_object_or_404(User, username=self.kwargs.get('username'))
-        return Post.objects.filter(author=user).order_by('-date_created')
+        queryset = Post.objects.filter(author=user)
+        context = super().get_context_data(**kwargs)
+        context['posts_list'] = queryset.order_by('-date_created')
+        return context
