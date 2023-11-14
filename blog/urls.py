2e2211de6bdf899187429ea147b066e8b87b6786
Anestezia-zip <plekatybtc@gmail.com>
2023-11-14 12:43:19 +0000

    Added 'about us' url

--- blog/urls.py
+++ blog/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('post/<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
     path('profile/', views.UserPostsView.as_view(), name='profile'),
     path('edit_profile/', views.edit_profile, name='edit_profile'),
+    path('about_us/', views.about_us, name='about_us'),
 ]
